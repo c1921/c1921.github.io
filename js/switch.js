7f9c6e7e3ad012fe701ab2e2bfe77f4961ae5beb
c1921 <w915504817@gmail.com>
2022-05-12 22:21:29 +0800

    侧边栏

---
+++ js/switch.js
@@ -0,0 +1,15 @@
+/*打开侧栏，修改侧栏宽度，主体左跨度、背景透明度*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "450px";
+    document.getElementById("main").style.marginLeft = "450px";
+    document.getElementById("open").style.fontSize = "0px"
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/*关闭侧栏，恢复原始侧栏宽度，主体左跨度、背景透明度*/
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("open").style.fontSize = "36px"
+    // document.body.style.backgroundColor = "white";
+}
