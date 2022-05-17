1d661d8930522d5366ff939cd096bccd44e6c7f5
c1921 <w915504817@gmail.com>
2022-05-17 23:07:51 +0800

    Update switch.js

--- js/switch.js
+++ js/switch.js
@@ -1,6 +1,6 @@
 /*打开侧栏，修改侧栏宽度，主体左跨度、背景透明度*/
 function openNav() {
-    document.getElementById("mySidenav").style.width = "450px";
+    document.getElementById("mySidenav").style.width = "400px";
     // document.getElementById("main").style.marginLeft = "450px";
     document.getElementById("open").style.width = "0px"
     // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
